054addbd288d2be31084258b4a9b0730c55f9da0
yinka <yinkaowo234@gmail.com>
2024-09-27 02:19:53 +0200

    done

---
+++ OneDrive/Desktop/ironhack/DOM-Intro/dom-intro/script.js
@@ -0,0 +1,7 @@
+
+const googleLink = document.getElementById('google-link');
+googleLink.setAttribute,("herf",'http://www.google.com');
+googleLink.setAttribute("herf");
+googleLink.setAttribute,("herf",'http://www.google.com');
+googleLink.setAttribute,("targert", "blank");
+googleLink.removeAttribute("class");
